b5cb62983e9090bbb92faf934942fba3268bbbeb
aherd2985 <andrew.herd@okstate.edu>
2020-09-11 21:36:02 -0500

    setup scanning for android

--- Todo.Android/MainActivity.cs
+++ Todo.Android/MainActivity.cs
@@ -4,18 +4,24 @@ using Android.Content.PM;
 
 namespace Todo
 {
-    [Activity(Label = "Herd", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true,
-        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity :  global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+  [Activity(Label = "Herd", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true,
+      ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+  public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+  {
+    protected override void OnCreate(Bundle bundle)
     {
-        protected override void OnCreate(Bundle bundle)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+      TabLayoutResource = Resource.Layout.Tabbar;
+      ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(bundle);
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
-        }
+      base.OnCreate(bundle);
+
+      ZXing.Net.Mobile.Forms.Android.Platform.Init();
+      global::Xamarin.Forms.Forms.Init(this, bundle);
+      LoadApplication(new App());
     }
+    public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+    {
+      global::ZXing.Net.Mobile.Forms.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+  }
 }
